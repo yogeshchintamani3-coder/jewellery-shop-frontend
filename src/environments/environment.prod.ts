ed6c45d29068928f866269ba9cd5c38ee1b29340
yogeshchintamani3-coder <yogesh.chintamani@kitaboo.com>
2026-04-28 10:25:07 +0530

    commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://jewellery-shop-backend-new.up.railway.app',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://jewellery-shop-backend-new.up.railway.app',
+};
